8cb5a1d2dcf9896c63030dbefd27d0ff4d66f5f6
Ilya Petrov <dnamix1@gmail.com>
2019-12-24 11:01:01 +0300

    added more products

--- src/context/productData.js
+++ src/context/productData.js
@@ -7,19 +7,19 @@ export const items = [
       company: "White Monkey Green Tea",
       description:
         "Baimao Hou is a green tea made from the leaves and bud of the green tea leaf when harvested during the first two weeks of the season (late March to early April). It originates from the Taimu Mountains in Fujian Province, China. The delicate leaves are carefully steamed and dried. The name originates from the appearance of the dried leaves, which are said to resemble the paw of a white-haired monkey. Due to the tea's appearance, flavor, and name, it is often mistaken for a white tea.",
-      featured: false,
+      featured: true,
       image: { fields: { file: { url: "img/whiteMonkey.jpg" } } }
     }
   },
   {
     sys: { id: 1 },
     fields: {
-      title: "Four Seasons of Spring Oolong",
+      title: "Four Seasons of Spring",
       price: "12.92",
       company: "Four Seasons Tea Company",
       description:
         "Four Seasons of Spring is named so because it produces four flushes (or harvests) each year that have a flavor and quality of that of a spring flush. This varietal was cultivated in Taiwan from a strain of TieGuanYin (Iron Goddess of Mercy), in the 1980s. This delightful oolong varietal has been cultivated for its sweet, floral flavors and expertly processed by hand. It is light yet buttery with lingering flowery finish of morning gardenias and warm milk.  Good for 2-4 infusions.Four Seasons of Spring is a good tea for beginning brewers. This tea is light, sweet and extremely fragrant.  The taste of gardenias clings to the upper palate and dazzles the senses.",
-      featured: false,
+      featured: true,
       image: { fields: { file: { url: "img/fourSeasonsSpring.jpg" } } }
     }
   },
@@ -57,6 +57,42 @@ export const items = [
         "Oolong Zhou Guy or Cinnamon from the mountains of Wuyi got its name due to its light but persistent aroma of cinnamon. Its taste is rich, tart, with delicate shades of wild honey, and the aroma is floral and woody. It grows on the mountainous cliffs of Wuyishan, whose soil is fertile and rich in mineral elements. After the fermentation process and a long roasting in an open oven it gets dark. When brewing its leaves acquire a yellow-green color with red veins.Tea was known at the Imperial court and is one of the most popular and favorite Oolong in China. You can drink it every day, as it has a great taste and has a positive effect on the whole body, stabilizes the cardiovascular and digestive system. It cleanses the lungs, liver, breaks down fat and removes toxins. This tea will quench your thirst, improve your mood.",
       featured: true,
       image: { fields: { file: { url: "img/cinnamonFromUi.jpg" } } }
+    }
+  },
+  {
+    sys: { id: 5 },
+    fields: {
+      title: "Dong Ding Oolong",
+      price: "29.40",
+      company: "Nan Tou, Taiwan",
+      description:
+        "Dong Ding Oolong is of the most famous and finest Formosa Oolong. The leaves of this tea are carefully hand-picked from Mount Dong Ding in Taiwan. It is believed that Dong Ding Oolong tea plants are actually a specimen of Wuyi strain that have been carefully selected and re-planted in Taiwan. As with most Taiwanese Oolongs, the frangrance and taste of this tea is wonderfully sweet and will leave you craving for more.",
+      featured: true,
+      image: { fields: { file: { url: "img/dongding.jpg" } } }
+    }
+  },
+  {
+    sys: { id: 6 },
+    fields: {
+      title: "Feng Huang Tuo Cha",
+      price: "48.60",
+      company: "Da Li Nan Jian Tea Industry",
+      description:
+        'This unconventional 250 grams Tou Cha is made from Da Ye Zhong tea leaves from Nan Jian Wu Liang Mountain in Yunnan province. On the box is the Chinese words "Da Feng Chao Yang", which translate to "Rising Phoenix". This is the 25th Anniversary Special Edition tea. As soon as you open the box you will notice the exceptionally strong fragrance. A very good Tuo Cha that can only get better as it ages.',
+      featured: true,
+      image: { fields: { file: { url: "img/fenghuangtuocha.jpg" } } }
+    }
+  },
+  {
+    sys: { id: 7 },
+    fields: {
+      title: "Pu-erh Chrysanthemum",
+      price: "7.90",
+      company: "Da Li Nan Jian Tea Industry",
+      description:
+        "This special blend of Pu-erh with Chrysanthemum flowers is very popular in Hong Kong. It is commonly served in many Chinese restaurants, especially during a Dim Sum meal. Not only is it delicious, but it is considered particularly good for cooling internal heat, as well as being able to aid digest the oily food. To ensure freshness, we will only create the blend when we received an order for this tea.",
+      featured: true,
+      image: { fields: { file: { url: "img/puehrjasmine.jpg" } } }
     }
   }
 ];
